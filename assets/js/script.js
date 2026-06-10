cdede68a2ee59197326cce51a7baa63ae5978e4b
yogiefernando14 <yogiefernando1401@gmail.com>
2026-06-10 14:10:36 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2894,33 +2894,29 @@ DELETE FILE
 ========================= */
 
 function bindDeleteFiles(){
-
-document
-.querySelectorAll(".delete-file")
-.forEach(btn=>{
-
-btn.addEventListener("click",()=>{
-
-const id =
-Number(btn.dataset.id);
+const file =
+files.find(
+f=>f.id===id
+);
 
 if(
 !confirm("Delete file?")
 ) return;
+
+if(file){
+
+addActivity(
+"File Deleted",
+file.name
+);
+
+}
 
 files =
 files.filter(
 file=>file.id!==id
 );
 
-saveFiles();
-renderFiles();
-
-});
-
-});
-
-}
 
 /* =========================
 UPLOAD FILE
